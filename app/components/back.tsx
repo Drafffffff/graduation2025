863d0d3bea34afbd463bb3ff876195fb040a49c8
drafff <i0.0ia@qq.com>
2025-05-24 18:40:17 +0800

    first page

---
+++ app/components/back.tsx
@@ -0,0 +1,9 @@
+export default function Back() {
+  return <div>
+    <div className="w-screen h-screen bg-white fixed">
+    </div>
+
+    <div className="w-[5rem] h-[5rem] bg-green-400 fixed">
+    </div>
+  </div>
+}
