4fb459755d709b9b0fdf4b610530e9b6fff57242
drafff <drafff@U-Q96PF4GL-1941.local>
2025-05-29 22:53:15 +0800

    changimg

--- app/routes/show.tsx
+++ app/routes/show.tsx
@@ -12,8 +12,8 @@ export default function Show() {
 
       <img id="img2" src="/show/2.png" className={`${styles.img2} absolute w-[6.5vw] top-[56vh] left-[25.2vw]`} />
 
-      <img id="img3" src="/show/3.png" className={`${styles.img3} absolute w-[8vw] top-[50vh] left-[40.85vw] z-[9]`} />
-      <img id="img4" src="/show/4.png" className={`${styles.img4} absolute w-[8vw] top-[56vh] left-[43.8vw] z-[8]`} />
+      <img id="img3" src="/show/4.png" className={`${styles.img3} absolute w-[8vw] top-[50vh] left-[40.85vw] z-[9]`} />
+      <img id="img4" src="/show/3.png" className={`${styles.img4} absolute w-[8vw] top-[56vh] left-[43.8vw] z-[8]`} />
       <img id="img5" src="/show/5.png" className={`${styles.img5} absolute w-[8vw] top-[46vh] left-[46.8vw] z-[7]`} />
       <img id="img6" src="/show/6.png" className={`${styles.img6} absolute w-[8vw] top-[56vh] left-[49.8vw] z-[6]`} />
       <img id="img7" src="/show/7.png" className={`${styles.img7} absolute w-[8vw] top-[56vh] left-[53.2vw] z-[5]`} />
